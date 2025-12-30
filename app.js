1ac46f96952bf8dbafdc6d33fbe4ffb3170f9f56
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2025-12-30 23:03:47 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+//Color Generator
+let para = document.getElementById("para")
+let rgb = ""
+function change(btn) {
+    btn.nextElementSibling.innerHTML = '<i class="fa-regular fa-copy"></i>'
+    let r = Math.floor(Math.random() * 257)
+    let g = Math.floor(Math.random() * 257)
+    let b = Math.floor(Math.random() * 257)
+    rgb = `rgb(${r}, ${g}, ${b})`
+    document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+    para.innerText = `Rgb code of the color is : rgb(${r}, ${g}, ${b})`
+}
+function copyRgb(btn) {
+    btn.innerHTML = '<i class="fa-solid fa-check"></i>'
+    navigator.clipboard.writeText(rgb)
+
+}
