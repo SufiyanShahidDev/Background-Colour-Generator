ca6f24ecfbc7e78ae2622ef09c2ed8b3387a8ef3
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2026-01-16 23:09:46 +0500

    Initial commit

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-//Color Generator
-let text = document.getElementById("text")
-let rgbCode = ""
-function colorSwitch(btn) {
-    btn.nextElementSibling.innerHTML = '<i class="fa-regular fa-copy"></i>'
-    let g = Math.floor(Math.random() * 255)
-    let r = Math.floor(Math.random() * 255)
-    let b = Math.floor(Math.random() * 255)
-    rgbCode = `rgb(${r}, ${g}, ${b})`
-    document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
-    text.innerText = `Rgb code of the color is : rgb(${r}, ${g}, ${b})`
-}
-function colorCode(btn) {
-    btn.innerHTML = '<i class="fa-solid fa-check"></i>'
-    navigator.clipboard.writeText(rgbCode)
-}
+// //Color Generator
+// let text = document.getElementById("text")
+// let rgbCode = ""
+// function colorSwitch(btn) {
+//     btn.nextElementSibling.innerHTML = '<i class="fa-regular fa-copy"></i>'
+//     let g = Math.floor(Math.random() * 255)
+//     let r = Math.floor(Math.random() * 255)
+//     let b = Math.floor(Math.random() * 255)
+//     rgbCode = `rgb(${r}, ${g}, ${b})`
+//     document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+//     text.innerText = `Rgb code of the color is : rgb(${r}, ${g}, ${b})`
+// }
+// function colorCode(btn) {
+//     btn.innerHTML = '<i class="fa-solid fa-check"></i>'
+//     navigator.clipboard.writeText(rgbCode)
+// }
